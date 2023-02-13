b9df16208f801aab2b76931bd4b455e279bcec33
Rustam Erkaev <erk_08@mail.ru>
2023-02-13 17:00:24 +0500

    feat: add Date and Time display

---
+++ js/time.js
@@ -0,0 +1,24 @@
+
+
+const time = document.querySelector('.time')
+const dateElem = document.querySelector('.date')
+
+
+function showTime() {
+    const date = new Date()
+    const currentTime = date.toLocaleTimeString()
+    time.textContent = currentTime
+    showDate()
+    setTimeout(showTime, 1000)
+}
+
+
+function showDate() {
+    const date = new Date()
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
+    const currentDate = date.toLocaleDateString('en-US', options);
+    dateElem.textContent = currentDate
+}
+
+
+showTime()
